48bd12311da952d8e229722bf9a98b2e513b3bff
Karthik Poojary <kpoojary20@gmail.com>
2017-01-05 21:39:33 +0530

    Day 3

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAhLDMemeA2q0k_Zpwlj1vRJSVu4Gk4q4o';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {videos:[]};
+    this.state = {
+      videos:[],
+      selectedVideo: null
+    };
     YTSearch({key:API_KEY, term:'Surfboard'}, (videos) => {
-      this.setState({videos})
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      });
     });
 
   }
@@ -19,7 +26,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos = {this.state.videos} />
+        <VideoDetail video = {this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos = {this.state.videos} />
       </div>
     );
   }
